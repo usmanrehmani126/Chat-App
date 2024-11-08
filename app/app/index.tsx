c90e7af3e7f47f81bcedb54e0d613de5995cca32
Usman <usmanrehmani126@gmail.com>
2024-11-08 11:20:37 -0800

    setup backend,add env's variables and setup app project

---
+++ app/app/index.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const Main = () => {
+  return (
+    <View>
+      <Text style={{color:'black',fontSize:30}}>Main</Text>
+    </View>
+  )
+}
+
+export default Main
+
+const styles = StyleSheet.create({})
